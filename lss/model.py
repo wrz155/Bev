42c762c1cec8777bb925008251d5b219a9fe75bc
wrz155 <wrz155_hfut@163.com>
2024-05-13 15:52:46 +0800

    Update model.py

--- lss/model.py
+++ lss/model.py
@@ -1,16 +1,42 @@
 import torch
 from torch import nn
+from torchvision.models.resnet import resnet18
+
+from efficientnet_pytorch import EfficientNet
 
 from .tools import gen_dx_bx
 
 
 class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, scale_factor=2):
+        super(Up, self).__init__()
+
+        self.up = nn.Upsample(scale_factor=scale_factor, mode='bilinear', align_corners=True)
+        self.conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+                                  nn.BatchNorm2d(out_channels),
+                                  nn.ReLU(inplace=True),
+                                  nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+                                  nn.BatchNorm2d(out_channels),
+                                  nn.ReLU(inplace=True))
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x1 = torch.cat([x2, x1], dim=1)
+        return self.conv(x1)
 
 
 class CamEncode(nn.Module):
+    def __init__(self, D, C, downsample):
+        super(CamEncode, self).__init__()
+        self.D = D
+        self.C = C
 
+        self.trunk = EfficientNet.from_pretrained("efficientnet-b0")
+        self.up1 = Up(320 + 112, 512)
+        self.depthnet = nn.Conv2d(512, self.D + self.C, kernel_size=1, padding=0)
 
-class BevEncode(nn.Module)
+class BevEncode(nn.Module):
+
 
 
 class LiftSplatShoot(nn.Module):
@@ -37,16 +63,31 @@ class LiftSplatShoot(nn.Module):
         # make grid in image plane
         ogfH, ogfW = self.data_aug_conf['final_dim']    # 128 * 352
         fH, fW = ogfH // self.downsample, ogfW // self.downsample  # 8 * 22
-        ds = torch.arange(*self.grid_conf['dbound'], dtype=torch.float).view(-1, 1, 1).expand(-1, fH, fW)
+        ds = torch.arange(*self.grid_conf['dbound'], dtype=torch.float).view(-1, 1, 1).expand(-1, fH, fW) # 41 * 8 * 22
         D, _, _ = ds.shape
         xs = torch.linspace(0, ogfW - 1, fW, dtype=torch.float).view(1, 1, fW).expand(D, fH, fW)
         ys = torch.linspace(0, ogfH - 1, fH, dtype=torch.float).view(1, fH, 1).expand(D, fH, fW)
 
-        # D x H x W x 3
+        # D x H x W x 3  [41, 8, 22, 3]
         frustum = torch.stack((xs, ys, ds), -1)
         return nn.Parameter(frustum, requires_grad=False)
 
     def get_geometry(self, rots, trans, intrins, post_rots, post_trans):
+        """Determine the (x,y,z) locations (in the ego frame)
+        of the points in the point cloud.
+        Returns B x N x D x H/downsample x W/downsample x 3
+        """
+        B, N, _ = trans.shape
+
+        # undo post-transformation
+        # B x N x D x H x W x 3
+        points = self.frustum - post_rots.view(B, N, 1, 1, 1, 3)
+
+
+
+
+
+
     def get_cam_feats(self, x):
     def voxel_pooling(self, geom_feats, x):
     def get_voxels(self, x, rots, trans, intrins, post_rots, post_trans):
